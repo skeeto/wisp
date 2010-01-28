bceab5f41d4b5d00d7c87b2ff21e1cec7d056975
Christopher Wellons <mosquitopsu@gmail.com>
2010-01-27 23:39:29 -0500

    Set up all 4 arithmetic functions.

--- lisp_math.c
+++ lisp_math.c
@@ -2,13 +2,70 @@
 #include <gmp.h>
 #include "wisp.h"
 
+typedef enum arith_enum
+{ ADD, SUB, MUL, DIV } arith_t;
+
 /* Maths */
-object_t *addition (object_t * lst)
+object_t *arith (arith_t op, object_t * lst)
 {
-  object_t *accumz = c_int (0);
-  object_t *accumf = c_float (0);
-  object_t *convf = c_float (0);
+  if (op == DIV)
+    REQM (lst, 2, c_sym ("div"));
   int intmode = 1;
+  object_t *accumz = NIL, *accumf = NIL, *convf = c_float (0);
+  switch (op)
+    {
+    case SUB:
+    case ADD:
+      accumz = c_int (0);
+      accumf = c_float (0);
+      break;
+    case MUL:
+    case DIV:
+      accumz = c_int (1);
+      accumf = c_float (1);
+      break;
+    }
+  if (op == SUB || op == DIV)
+    {
+      object_t *num = CAR (lst);
+      if (FLOATP (num))
+	{
+	  intmode = 0;
+	  mpf_set (DFLOAT (accumf), DFLOAT (num));
+	}
+      else if (INTP (num))
+	{
+	  mpf_set_z (DFLOAT (convf), DINT (num));
+	  mpf_set (DFLOAT (accumf), DFLOAT (convf));
+	  mpz_set (DINT (accumz), DINT (num));
+	}
+      else
+	{
+	  obj_destroy (accumz);
+	  obj_destroy (accumf);
+	  obj_destroy (convf);
+	  THROW (wrong_type, UPREF (num));
+	}
+      if (op == SUB && CDR (lst) == NIL)
+	{
+	  if (intmode)
+	    {
+	      obj_destroy (accumf);
+	      obj_destroy (convf);
+	      mpz_neg (DINT (accumz), DINT (accumz));
+	      return accumz;
+	    }
+	  else
+	    {
+	      obj_destroy (accumz);
+	      obj_destroy (convf);
+	      mpf_neg (DFLOAT (accumf), DFLOAT (accumf));
+	      return accumf;
+	    }
+	}
+      lst = CDR (lst);
+    }
+
   while (lst != NIL)
     {
       object_t *num = CAR (lst);
@@ -16,6 +73,7 @@ object_t *addition (object_t * lst)
 	{
 	  obj_destroy (accumz);
 	  obj_destroy (accumf);
+	  obj_destroy (convf);
 	  THROW (wrong_type, UPREF (num));
 	}
 
@@ -28,17 +86,61 @@ object_t *addition (object_t * lst)
 	      /* Fall through to !intmode */
 	    }
 	  else if (INTP (num))
-	    mpz_add (DINT (accumz), DINT (accumz), DINT (CAR (lst)));
+	    {
+	      switch (op)
+		{
+		case ADD:
+		  mpz_add (DINT (accumz), DINT (accumz), DINT (CAR (lst)));
+		  break;
+		case MUL:
+		  mpz_mul (DINT (accumz), DINT (accumz), DINT (CAR (lst)));
+		  break;
+		case SUB:
+		  mpz_sub (DINT (accumz), DINT (accumz), DINT (CAR (lst)));
+		  break;
+		case DIV:
+		  mpz_div (DINT (accumz), DINT (accumz), DINT (CAR (lst)));
+		  break;
+		}
+	    }
 	}
       if (!intmode)
 	{
 	  if (FLOATP (num))
-	    mpf_add (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+	    switch (op)
+	      {
+	      case ADD:
+		mpf_add (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		break;
+	      case MUL:
+		mpf_mul (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		break;
+	      case SUB:
+		mpf_sub (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		break;
+	      case DIV:
+		mpf_div (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		break;
+	      }
 	  else if (INTP (num))
 	    {
 	      /* Convert to float and add. */
 	      mpf_set_z (DFLOAT (convf), DINT (num));
-	      mpf_add (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+	      switch (op)
+		{
+		case ADD:
+		  mpf_add (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		  break;
+		case MUL:
+		  mpf_mul (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		  break;
+		case SUB:
+		  mpf_sub (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		  break;
+		case DIV:
+		  mpf_div (DFLOAT (accumf), DFLOAT (accumf), DFLOAT (num));
+		  break;
+		}
 	    }
 	}
       lst = CDR (lst);
@@ -55,8 +157,31 @@ object_t *addition (object_t * lst)
   return accumf;
 }
 
+object_t *addition (object_t * lst)
+{
+  return arith (ADD, lst);
+}
+
+object_t *multiplication (object_t * lst)
+{
+  return arith (MUL, lst);
+}
+
+object_t *subtraction (object_t * lst)
+{
+  return arith (SUB, lst);
+}
+
+object_t *division (object_t * lst)
+{
+  return arith (DIV, lst);
+}
+
 /* Install all the math functions */
 void lisp_math_init ()
 {
   SSET (c_sym ("+"), c_cfunc (&addition));
+  SSET (c_sym ("*"), c_cfunc (&multiplication));
+  SSET (c_sym ("-"), c_cfunc (&subtraction));
+  SSET (c_sym ("/"), c_cfunc (&division));
 }
